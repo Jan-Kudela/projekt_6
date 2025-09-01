d1e21d90063ab69b5898188ccb1f4a94d1093c36
Jan-Kudela <jochanan.jorgen@gmail.com>
2025-09-01 21:40:43 +0200

    zápis druhého testu na otevření okna s přihlášením

--- test_kytary.py
+++ test_kytary.py
@@ -8,13 +8,40 @@ def page():
         page = browser.new_page()
         yield page
 
-
-def test_cookies(page: Page):
-
+def cookie_bar_accept():
     page.goto("https://kytary.cz/")
-    decline_button = page.locator("a.btn.btn-success")
-    decline_button.click()
+    accept_button = page.locator(
+        "#cpModal > div > div > div > div.btns > a.btn.btn-success"
+        )
+    accept_button.click()
+    yield
 
-    cookie_bar = page.locator("#cpModal > div > div > div")
+
+def test_accept_cookies(page: Page):
+    '''Testuje, zda po kliknuti na tlačítko "Povolit vše", zmizí cookie bar'''
+    
+    cookie_bar_accept()
+    
+    cookie_bar = page.locator("#modal-body")
 
     assert cookie_bar.is_visible() == False
+
+
+def test_login_window(page: Page):
+    '''Testuje, zda se po kliknuti na tlacitko Můj účet
+      zobrazi prihlasovaci okno'''
+    
+    page.goto("https://kytary.cz/")
+    cookies_ok = page.locator(
+        "#cpModal > div > div > div > div.btns > a.btn.btn-success"
+        )
+    cookies_ok.click()
+
+    account_button = page.locator(".m-icons a:nth-of-type(3) .desc")
+    account_button.click()
+
+    login_window = page.locator("#loginModal .modal-title")
+
+    assert login_window.is_visible() == True
+
+
